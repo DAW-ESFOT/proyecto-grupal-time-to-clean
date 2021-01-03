e5f79cb5672bd0e9a634c62f668bb3e56c6b8fe5
DESKTOP-VFUJHKU\gaby2 <gaby.2ax.88@gmail.com>
2021-01-02 20:37:02 -0500

    #7 #24 Rutas y controlador del modelo Complaint

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Models\Neighborhoods;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\User;
+use App\Models\Complaint;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +17,33 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+/*Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+*/
+//Users-Drivers
+Route::post('register', 'App\Http\Controllers\UserController@register');
+Route::post('login', 'App\Http\Controllers\UserController@authenticate');
+
+Route::get('neighborhoods', 'App\Http\Controllers\NeighborhoodController@index');
+Route::get('neighborhoods/{neighborhood}', 'App\Http\Controllers\NeighborhoodController@show');
+
+Route::post('complaints', 'App\Http\Controllers\ComplaintController@store');
+
+Route::group(['middleware' => ['jwt.verify']], function() {
+    Route::get('users', 'App\Http\Controllers\UserController@getAuthenticatedUser');
+    Route::get('users/{user}', 'App\Http\Controllers\UserController@show');
+    Route::post('users', 'App\Http\Controllers\UserController@store');
+    Route::put('users/{user}', 'App\Http\Controllers\UserController@update');
+    Route::delete('users/{user}', 'App\Http\Controllers\UserController@delete');
+
+    Route::post('neighborhoods', 'App\Http\Controllers\NeighborhoodController@store');
+    Route::put('neighborhoods/{neighborhood}', 'App\Http\Controllers\NeighborhoodController@update');
+    Route::delete('neighborhoods/{neighborhood}', 'App\Http\Controllers\NeighborhoodController@delete');
+
+    Route::get('complaints', 'App\Http\Controllers\ComplaintController@index');
+    Route::get('complaints/{complaint}', 'App\Http\Controllers\ComplaintController@show');
+    Route::put('complaints/{complaint}', 'App\Http\Controllers\ComplaintController@update');
+    Route::delete('complaints/{complaint}', 'App\Http\Controllers\ComplaintController@delete');
+});
+
