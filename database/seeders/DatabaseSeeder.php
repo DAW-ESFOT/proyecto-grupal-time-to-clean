2bb5f568b8bb4af6b850828b02b48ab7016a5b98
Eddy Hipo <eddy.hipo@epn.edu.ec>
2021-01-03 15:15:49 -0500

    cambios en el dataseeder y truckseeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,5 +18,6 @@ class DatabaseSeeder extends Seeder
         $this->call(UsersTableSeeder::class);
         $this->call(ComplaintsTableSeeder::class);
         $this->call(NeighborhoodsTableSeeder::class);
+        $this->call(TrucksTableSeeder::class);
     }
 }
