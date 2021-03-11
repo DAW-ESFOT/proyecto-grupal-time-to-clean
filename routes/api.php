f31a5777f404ef56702ab315cce87765ea376de5
DESKTOP-VFUJHKU\gaby2 <gaby.2ax.88@gmail.com>
2021-03-11 11:43:27 -0500

    Filtrado por stateComplaint

--- routes/api.php
+++ routes/api.php
@@ -25,7 +25,7 @@ use App\Models\Complaint;
 
 Route::post('login', 'App\Http\Controllers\UserController@authenticate');
 
-Route::get('neighborhoods/all', 'App\Http\Controllers\NeighborhoodController@showAll');
+Route::get('neighborhoods', 'App\Http\Controllers\NeighborhoodController@index');
 Route::get('neighborhoods/{neighborhood}', 'App\Http\Controllers\NeighborhoodController@show');
 Route::post('complaints', 'App\Http\Controllers\ComplaintController@store');
 
@@ -35,7 +35,6 @@ Route::group(['middleware' => ['jwt.verify']], function() {
     Route::post('logout', 'App\Http\Controllers\UserController@logout');
 
     Route::get('users', 'App\Http\Controllers\UserController@index');
-    Route::get('users/all', 'App\Http\Controllers\UserController@index');
     Route::get('user', 'App\Http\Controllers\UserController@getAuthenticatedUser');
     Route::get('users/{user}', 'App\Http\Controllers\UserController@show');
     Route::get('users/filter/alternate', 'App\Http\Controllers\UserController@showDriversAlternate');
@@ -47,7 +46,7 @@ Route::group(['middleware' => ['jwt.verify']], function() {
 
     //Neighborhods
 
-    Route::get('neighborhoods', 'App\Http\Controllers\NeighborhoodController@index');
+
     Route::get('neighborhoods/filter/without-trucks', 'App\Http\Controllers\NeighborhoodController@showNeighborhoodsWithoutTruck');
     Route::get('neighborhoods/filter/without-complaints', 'App\Http\Controllers\NeighborhoodController@showNeighborhoodsWithoutComplaints');
     Route::post('neighborhoods', 'App\Http\Controllers\NeighborhoodController@store');
@@ -56,17 +55,19 @@ Route::group(['middleware' => ['jwt.verify']], function() {
 
     //Complaints
     Route::get('complaints', 'App\Http\Controllers\ComplaintController@index');
-    Route::get('complaints/all', 'App\Http\Controllers\ComplaintController@showAll');
     Route::get('complaints/{complaint}', 'App\Http\Controllers\ComplaintController@show');
     Route::get('drivers/filter/with-complaints', 'App\Http\Controllers\ComplaintController@showDriversWithComplaints');
     Route::get('trucks/filter/with-complaints', 'App\Http\Controllers\ComplaintController@showTrucksWithComplaints');
     Route::get('neighborhoods/filter/with-complaints', 'App\Http\Controllers\ComplaintController@showNeighborhoodsWithComplaints');
     Route::put('complaints/{complaint}', 'App\Http\Controllers\ComplaintController@update');
     Route::delete('complaints/{complaint}', 'App\Http\Controllers\ComplaintController@delete');
+    Route::get('complaints/filter/state1', 'App\Http\Controllers\ComplaintController@findStatePendiente');
+    Route::get('complaints/filter/state2', 'App\Http\Controllers\ComplaintController@findStateProceso');
+    Route::get('complaints/filter/state3', 'App\Http\Controllers\ComplaintController@findStateAtendida');
+
 
     //Trucks
     Route::get('trucks', 'App\Http\Controllers\TruckController@index');
-    Route::get('trucks/all', 'App\Http\Controllers\TruckController@showAll');
     Route::get('trucks/{truck}', 'App\Http\Controllers\TruckController@show');
     Route::get('trucks/filter/working', 'App\Http\Controllers\TruckController@showTrucksWorking');
     Route::get('trucks/filter/no-working', 'App\Http\Controllers\TruckController@showTrucksNoWorking');
